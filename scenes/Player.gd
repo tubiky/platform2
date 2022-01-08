23c64733c33a2492f2a8fbb21480ad1b72586d90
tubiky <61419018+tubiky@users.noreply.github.com>
2022-01-08 20:09:34 +0900

    section2-12:Adding Collectable

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,19 +2,18 @@ extends KinematicBody2D
 
 var gravity = 1000
 var velocity = Vector2.ZERO
-var maxHorizontalSpeed = 100
+var maxHorizontalSpeed = 140
 var horizontalAcceleration = 1500
 var jumpSpeed = 300
 var jumpTerminationMultiplier = 3
+var hasDoubleJump = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	var moveVector = Vector2.ZERO
-	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	var moveVector = get_movement_vector()
 	
 	velocity.x += moveVector.x * horizontalAcceleration * delta
 	
@@ -24,11 +23,46 @@ func _process(delta):
 		
 	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
 	
-	if (moveVector.y < 0 && is_on_floor()):
+	if (moveVector.y < 0 && (is_on_floor() || !$CoyoteTimer.is_stopped() || hasDoubleJump)):
 		velocity.y = moveVector.y * jumpSpeed
+		if (!is_on_floor() && $CoyoteTimer.is_stopped()):
+			hasDoubleJump = false
+		$CoyoteTimer.stop()
 		
 	if (velocity.y < 0 && !Input.is_action_pressed("jump")):
 		velocity.y += gravity * jumpTerminationMultiplier * delta
 	else:
 		velocity.y += gravity * delta
+		
+	var wasOnFloor = is_on_floor()
+	
+	# move_and_slide updates the is_on_floor state internally
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if (wasOnFloor && !is_on_floor()):
+		$CoyoteTimer.start()
+		
+	if (is_on_floor()):
+		hasDoubleJump = true
+	
+	update_animation()
+
+func get_movement_vector():
+	var moveVector = Vector2.ZERO
+	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	
+	return moveVector
+
+func update_animation():
+	var moveVec = get_movement_vector()
+	
+	if (!is_on_floor()):
+		$AnimatedSprite.play("jump")
+	elif (moveVec.x != 0):
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+
+	if (moveVec.x != 0):
+		$AnimatedSprite.flip_h = true if moveVec.x > 0 else false
